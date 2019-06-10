199b9c7267cb29161a2825809acfad9529117aad
maobow <997526931@qq.com>
2019-06-10 08:55:35 +0800

    first commit

---
+++ js/js/koala.js
@@ -0,0 +1,48 @@
+			var srr = [{
+						"src": "img/koala/1.jpg",
+						"name": "薏仁水",
+						"inf": "日本进口",
+						"price": "56￥"
+					},
+					{
+						"src": "img/koala/2.jpg",
+						"name": "VC水",
+						"inf": "泰国进口",
+						"price": "80￥"
+					},
+					{
+						"src": "img/koala/3.jpg",
+						"name": "野城医生水",
+						"inf": "日本进口",
+						"price": "100￥"
+					},
+					{
+						"src": "img/koala/4.jpg",
+						"name": "口红套装",
+						"inf": "美国进口",
+						"price": "230￥"
+					},
+					{
+						"src": "img/koala/5.jpg",
+						"name": "swisse叶绿素",
+						"inf": "澳洲进口",
+						"price": "99￥"
+					},
+					{
+						"src": "img/koala/6.jpg",
+						"name": "skII精华",
+						"inf": "日本进口",
+						"price": "1230￥"
+					},
+					{
+						"src": "img/koala/7.jpg",
+						"name": "欣兰面膜",
+						"inf": "中国台湾",
+						"price": "100￥"
+					},
+					{
+						"src": "img/koala/8.jpg",
+						"name": "艾医生素颜霜",
+						"inf": "韩国进口",
+						"price": "160￥"
+					}]
